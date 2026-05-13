610716996192a89729e173deb0fad1eadbc50dd8
Rene Catalan <reene.ddyyfmen@gmail.com>
2026-05-13 22:58:17 +0200

    Fix GitHub Pages base path and rebuild assets

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig(({ mode }) => {
   // process requiere npm install -D @types/node
   const env = loadEnv(mode, process.cwd(), '')
   return {
-    base: env.VITE_APP_BASE_URL || '/Charts/',
+    base: env.VITE_APP_BASE_URL || '/ChartsYoutubeSpotify/',
     build:{
       outDir:'docs',
     },
